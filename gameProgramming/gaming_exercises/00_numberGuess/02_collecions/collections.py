95693eb404f7dc57ae059676d4ce0572e6da49d8
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-10-17 14:27:44 -0400

    v0.1

--- gameProgramming/gaming_exercises/00_numberGuess/02_collecions/collections.py
+++ gameProgramming/gaming_exercises/00_numberGuess/02_collecions/collections.py
@@ -1,16 +1,30 @@
-playerInventory = []
+# # playerInventory = []
 
-while len(playerInventory) < 10: 
-    item = input("What item do you want to add to the inventory?\n")
-    playerInventory.append(item)
+# # while len(playerInventory) < 10: 
+# #     item = input("What item do you want to add to the inventory?\n")
+# #     playerInventory.append(item)
 
-playerInventory.sort()
-print(playerInventory)
+# # playerInventory.sort()
+# # print(playerInventory)
 
-while len(playerInventory) > 5:
-    input("What item do you want to add to the inventory?\n")
-    playerInventory.remove(item)
+# # while len(playerInventory) > 5:
+# #     input("What item do you want to add to the inventory?\n")
+# #     playerInventory.remove(item)
 
-playerInventory.sort()
-print(playerInventory)
+# # playerInventory.sort()
+# # print(playerInventory)
 
+# doorKeys = [
+#     "red",
+#     "green",
+#     "yellow",
+#     "purple",
+#     "browns"
+# ]
+
+# keys = input("which color key doyou need to unlock the door?\n")
+
+# if key in doorKeys:
+#     print("You have the correct key! The door unlocks.\n")
+# else:
+#     print("You do not have that key. The door remain locked\n")
