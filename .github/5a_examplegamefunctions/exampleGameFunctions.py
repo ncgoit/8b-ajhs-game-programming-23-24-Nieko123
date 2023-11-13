16a788e29a1f32d10803d10b1851bae06dcb3e87
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-11-13 14:45:19 -0500

    v

--- .github/5a_examplegamefunctions/exampleGameFunctions.py
+++ .github/5a_examplegamefunctions/exampleGameFunctions.py
@@ -1,1 +1,28 @@
 # Example Game Function project, Nieko Garnes v0.0
+import random
+
+def startRace():
+    pass
+
+def raceDistance(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def raceOutcome(param1, param2, param3):
+    pass
+
+def raceOutcome(laps, speed, place):
+    if laps > 1 and speed >= 50 and place == 'Won':
+        raceOutcome = True
+    elif laps > 1 and speed >= 50 and place == 'lose':
+        raceOutcome = False
+    else:
+        print(' Aww man, You lose!\n')
+        raceLose = True
+        return raceLose
+    return raceOutcome
+
+
+
