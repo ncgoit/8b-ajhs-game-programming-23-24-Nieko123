2b259bd520936081e8c9d7e6dc586d11a36fff12
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-11-30 13:44:36 -0500

    v0.4

--- 03b_functions/function.py
+++ 03b_functions/function.py
@@ -1,3 +1,4 @@
+import random
 # Function -- a named piece of code that can be rueses easily.
 # FUNCTION SIGNATURE -- Syntax for creating a new function
 def exampleFunctionA():
@@ -15,7 +16,24 @@ def exampleFunctionB(num, count):
 # IF A FUNCTION REQUIRES PARAMETERS, YOUR CODE WILL CRASH WITHOUT THEM!
 
 # RUNNING A FUNCTION IS KNOWN AS CALLING THE FUNCTION
-exampleFunctionA()
-exampleFunctionB(5, 0)
+# exampleFunctionA()
+# exampleFunctionB(5, 0)
 
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        numRolled += 1
 
+rollDice(3, 6)
+# rollDice(1, 20)
+
+strengthPlayer = rollDice(3, 6)
+dexterityPlayer = rollDice(3, 6)
+wisdom Player =  rollDice(3, 6)
+
+print()
+print()
+print()
